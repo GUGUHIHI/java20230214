159f95f5ffcde88009b38eaea7c0e999b650c82f
GUGUHIHI <123609789+GUGUHIHI@users.noreply.github.com>
2023-02-22 18:24:20 +0900

    20230222

---
+++ src/ch05/lecture/p02reference/C01Reference.java
@@ -0,0 +1,9 @@
+package ch05.lecture.p02reference;
+
+public class C01Reference {
+	public static void main(String[] args) {
+		int a = 3; // 기본 타입
+		
+		int[] arr = new int[] {3, 4, 5}; // 참조 타입
+	}
+}
