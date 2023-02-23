484671ae42746079b2427dcae11863861d665e73
GUGUHIHI <123609789+GUGUHIHI@users.noreply.github.com>
2023-02-23 17:47:50 +0900

    20230223

---
+++ src/baekjoon/Exam1152.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Exam1152 {
+	public static void main(String[] args) {
+		String input3 = "The last character is a blank";
+
+		Scanner scanner = new Scanner(input3);
+
+		String line = scanner.nextLine();
+
+		String[] arr = line.split(" ");
+		int answer = 0;
+
+		for (int i = 0; i < arr.length; i++)
+			
+		{
+			if (!arr[i].equals("")) {
+				answer++;
+			}
+		}
+		System.out.println(answer);
+	}
+}
---
+++ src/ch05/Exercise/Example07.java
@@ -0,0 +1,21 @@
+package ch05.Exercise;
+
+public class Example07 {
+	public static void main(String[] args) {
+
+		int answer = Integer.MIN_VALUE;
+
+		int[] arr = { -1, -5, -3, -8, -2 };
+
+		for (int i = 0; i < arr.length; i++) {
+
+			if (answer < arr[i]) {
+				// 내가 알고 있는수보다 크면?
+				answer = arr[i];
+			}
+			// 그렇지 않으면?
+		}
+
+		System.out.println(answer);
+	}
+}
