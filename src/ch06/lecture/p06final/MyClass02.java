bc05e2ce28dc06a7b2a2c2e2695550dcfbe66b31
GUGUHIHI <123609789+GUGUHIHI@users.noreply.github.com>
2023-02-28 17:39:08 +0900

    20230228

---
+++ src/ch06/lecture/p06final/MyClass02.java
@@ -0,0 +1,10 @@
+package ch06.lecture.p06final;
+
+public class MyClass02 {
+	int i;
+	final int j = 3;
+	
+	static int k;
+	final static int L = 99;
+	static final int M = 100; // static final의 변수명은 SNAKE_CASE
+}
---
+++ src/ch06/lecture/p06final/MyClass03.java
@@ -0,0 +1,6 @@
+package ch06.lecture.p06final;
+
+public class MyClass03 {
+	final static int L = 99;
+	static final int M = 100; // static final의 변수명은 SNAKE_CASE
+}
