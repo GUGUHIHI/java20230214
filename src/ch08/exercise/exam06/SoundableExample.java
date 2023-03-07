7eaee93fbc3fc5cf1d27b122e892518e5a20ed99
GUGUHIHI <123609789+GUGUHIHI@users.noreply.github.com>
2023-03-07 17:45:35 +0900

    20230307

---
+++ src/ch08/exercise/exam06/SoundableExample.java
@@ -0,0 +1,14 @@
+package ch08.exercise.exam06;
+
+import ch07.sec10.exam02.*;
+
+public class SoundableExample {
+	public static void printSound(Soundable soundable) {
+		System.out.println(soundable.sound());
+	}
+	
+	public static void main(String[] args) {
+		printSound(new Cat());
+		printSound(new Dog());
+	}
+}
---
+++ src/ch08/exercise/exam07/DataAccessObject.java
@@ -0,0 +1,13 @@
+package ch08.exercise.exam07;
+
+public interface DataAccessObject {
+
+	void select();
+
+	void insert();
+
+	void update();
+
+	void delete();
+
+}
